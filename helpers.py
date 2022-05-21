d3fbda01fe19940d1f02cea6b5c926ef875c6ea7
brofrain <downloading130@gmail.com>
2022-05-21 22:25:53 +0200

    format printed decimals

--- helpers.py
+++ helpers.py
@@ -10,11 +10,11 @@ reversed_sorted_samples = sorted_samples.reverse()
 def print_sorting_time(label: str, sort_cb: callable(int)):
     print(f"# {label}")
     print(
-        f" - random samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER)}"
+        f" - random samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
     )
     print(
-        f" - sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER)}"
+        f" - sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
     )
     print(
-        f" - reversed sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER)}"
+        f" - reversed sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
     )
