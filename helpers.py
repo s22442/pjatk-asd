ed048e3fbd5056ea0e98c946f2fa10c17bb9a334
brofrain <downloading130@gmail.com>
2022-05-21 23:12:46 +0200

    fix helper

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ from config import SAMPLE_VALUE_RANGE, SAMPLE_COUNT, TIMEIT_NUMBER
 
 random_samples = [round(random() * SAMPLE_VALUE_RANGE + 1) for _ in range(SAMPLE_COUNT)]
 sorted_samples = [i for i in range(SAMPLE_COUNT)]
-reversed_sorted_samples = sorted_samples.reverse()
+reversed_sorted_samples = sorted_samples[:].reverse()
 
 
 def print_sorting_time(label: str, sort_cb: callable(int)):
@@ -13,8 +13,8 @@ def print_sorting_time(label: str, sort_cb: callable(int)):
         f" - random samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
     )
     print(
-        f" - sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
+        f" - sorted samples: {timeit(lambda: sort_cb(sorted_samples), number = TIMEIT_NUMBER):.6f}"
     )
     print(
-        f" - reversed sorted samples: {timeit(lambda: sort_cb(random_samples), number = TIMEIT_NUMBER):.6f}"
+        f" - reversed sorted samples: {timeit(lambda: sort_cb(reversed_sorted_samples), number = TIMEIT_NUMBER):.6f}"
     )
